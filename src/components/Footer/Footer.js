99380061abefc03cec5b76fcb236a12b5deec0f6
Amr-Ezz <amroelghitany@gmail.com>
2023-06-26 20:18:09 +0300

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,46 +2,48 @@ import React from "react";
 import { MDBFooter, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
 import "./Footer.css";
 const Footer = () => {
-    
   return (
     <MDBFooter
-      className="text-center text-white py-4"
-      style={{ backgroundColor: "#00000092" }}
+      className="text-center text-white py-1"
+      style={{ backgroundColor: "rgba(42, 0, 0, 0.69)" }}
     >
       <MDBContainer>
         <section>
           <MDBRow>
-            <MDBCol lg="12" style={{padding: "0"}}>
-            <div className="video-wrapper">
-            <div className="ratio ratio-16x9">
-               
-               <video
-                //  className="shadow-1-strong rounded"
-                 src="/anime_-_103434 (540p).mp4"
-                 title="local video"
-                 loop
-                 autoPlay
-                 muted
-                 playsInline
-                 style={{width: "100%"}}
-               
-                 
-               ></video>
-             </div>
-                    </div>
-             
+            <MDBCol lg="12" style={{ padding: "0" }}>
+              <div className="video-wrapper">
+                <div className="ratio ratio-16x9">
+                  <video
+                    className="shadow-1-strong rounded"
+                    src="/anime_-_103434 (540p).mp4"
+                    title="local video"
+                    loop
+                    autoPlay
+                    muted
+                    playsInline
+                    style={{ width: "100%" }}
+                  ></video>
+                </div>
+              </div>
             </MDBCol>
           </MDBRow>
         </section>
       </MDBContainer>
+      <div className="footer_container">
+        <img src="anime_footer.jpg" alt="anime image" className="anime_footer_img" />
+      </div>
 
       <div
         className="text-center p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
         © 2023 Copyright:
-        <a className="text-white" style={{background: "none", padding: "0.5rem"}} href="https://github.com/Amr-Ezz">
-        https://github.com/Amr-Ezz
+        <a
+          className="text-white"
+          style={{ background: "none", padding: "0.5rem" }}
+          href="https://github.com/Amr-Ezz"
+        >
+          https://github.com/Amr-Ezz
         </a>
       </div>
     </MDBFooter>
@@ -49,3 +51,4 @@ const Footer = () => {
 };
 
 export default Footer;
+/*  */
